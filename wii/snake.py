8e3dac78a444590dd2369c3c702818752eefbd2a
Pieter Vleminckx <pieter.vlem@gmail.com>
2018-10-21 16:52:40 +0200

    update snake

--- wii/snake.py
+++ wii/snake.py
@@ -9,59 +9,91 @@
 
 import random
 import curses
+import cwiid
+import time
 
-s = curses.initscr()
-curses.curs_set(0)
-sh, sw = s.getmaxyx()
-w = curses.newwin(sh, sw, 0, 0)
-w.keypad(1)
-w.timeout(100)
+def main():
+    print 'Press button 1 + 2 on your Wii Remote...'
+    time.sleep(1)
 
-snk_x = sw/4
-snk_y = sh/2
-snake = [
-    [snk_y, snk_x],
-    [snk_y, snk_x-1],
-    [snk_y, snk_x-2]
-]
+    wm=cwiid.Wiimote()
+    time.sleep(1)
+	
+	Rumble = False
+    wm.rpt_mode = cwiid.RPT_BTN
+	
+	position = 50
+	print 'starting position: ', position
 
-food = [sh/2, sw/2]
-w.addch(food[0], food[1], curses.ACS_PI)
+    s = curses.initscr()
+    curses.curs_set(0)
+    sh, sw = s.getmaxyx()
+    w = curses.newwin(sh, sw, 0, 0)
+    w.keypad(1)
+    w.timeout(100)
 
-key = curses.KEY_RIGHT
+    snk_x = sw/4
+    snk_y = sh/2
+    snake = [
+        [snk_y, snk_x],
+        [snk_y, snk_x-1],
+        [snk_y, snk_x-2]
+    ]
 
-while True:
-    next_key = w.getch()
-    key = key if next_key == -1 else next_key
+    food = [sh/2, sw/2]
+    w.addch(food[0], food[1], curses.ACS_PI)
 
-    if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
-        curses.endwin()
-        quit()
+    key = curses.KEY_RIGHT
 
-    new_head = [snake[0][0], snake[0][1]]
+    while True:
+        next_key = w.getch()
+        key = key if next_key == -1 else next_key
 
-    if key == curses.KEY_DOWN:
-        new_head[0] += 1
-    if key == curses.KEY_UP:
-        new_head[0] -= 1
-    if key == curses.KEY_LEFT:
-        new_head[1] -= 1
-    if key == curses.KEY_RIGHT:
-        new_head[1] += 1
+        if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
+            curses.endwin()
+            quit()
 
-    snake.insert(0, new_head)
+        new_head = [snake[0][0], snake[0][1]]
 
-    if snake[0] == food:
-        food = None
-        while food is None:
-            nf = [
-                random.randint(1, sh-1),
-                random.randint(1, sw-1)
-            ]
-            food = nf if nf not in snake else None
-        w.addch(food[0], food[1], curses.ACS_PI)
-    else:
-        tail = snake.pop()
-        w.addch(tail[0], tail[1], ' ')
+        
+        #if key == curses.KEY_DOWN:
+        if wm.state['buttons'] == 256:
+            new_head[0] += 1
+        #if key == curses.KEY_UP:
+        if wm.state['buttons'] == 512:
+            new_head[0] -= 1
+        #if key == curses.KEY_LEFT:
+        if wm.state['buttons'] == 2048:
+            new_head[1] -= 1
+        #if key == curses.KEY_RIGHT:
+        if wm.state['buttons'] == 1024:
+            new_head[1] += 1
 
-    w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
+        if key == curses.KEY_DOWN:
+            new_head[0] += 1
+        if key == curses.KEY_UP:
+            new_head[0] -= 1
+        if key == curses.KEY_LEFT:
+            new_head[1] -= 1
+        if key == curses.KEY_RIGHT:
+            new_head[1] += 1
+
+        snake.insert(0, new_head)
+
+        if snake[0] == food:
+            food = None
+            while food is None:
+                nf = [
+                    random.randint(1, sh-1),
+                    random.randint(1, sw-1)
+                ]
+                food = nf if nf not in snake else None
+            w.addch(food[0], food[1], curses.ACS_PI)
+        else:
+            tail = snake.pop()
+            w.addch(tail[0], tail[1], ' ')
+
+        w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
+
+if __name__ == '__main__':
+    main()
